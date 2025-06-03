165e2e9e4af854a6e6591e393811a9398b7b3c64
N9 <nguyengiapham1@gmail.com>
2025-06-04 01:17:01 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'Puncture Vehicle Tires with n9-target using nail item and sync'
-author 'yourname'
+author 'N9 Development'
 version '1.0.0'
 
 client_scripts {
