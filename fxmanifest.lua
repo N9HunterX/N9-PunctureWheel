d044e0db16b969dca291730abcc514ddff572ac8
N9 <nguyengiapham1@gmail.com>
2025-06-04 01:16:15 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Puncture Vehicle Tires with n9-target using nail item and sync'
+author 'yourname'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'qb-core',
+    'qb-target',
+    'qb-inventory'
+}
